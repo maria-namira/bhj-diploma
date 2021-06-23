1666410f12996c0791866d81c8a07d9b9b572d0f
Maria <mariaes2016@yandex.ru>
2021-06-23 11:21:23 +0300

    diploma

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -3,29 +3,51 @@
  * Имеет свойство URL, равно пустой строке.
  * */
 class Entity {
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static list(data, callback){
 
-  }
+    static URL = '';
+    
+    /**
+     * Запрашивает с сервера список данных.
+     * Это могут быть счета или доходы/расходы
+     * (в зависимости от того, что наследуется от Entity)
+     * */
+    static list(data, callback) {
+        createRequest({
+            url: this.URL,
+            method: 'GET',
+            responseType: 'json',
+            data,
+            callback
+        });
+    }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
-  static create(data, callback) {
+    /**
+     * Создаёт счёт или доход/расход с помощью запроса
+     * на сервер. (в зависимости от того,
+     * что наследуется от Entity)
+     * */
+    static create(data, callback) {
+        createRequest({
+            url: this.URL,
+            method: 'PUT',
+            responseType: 'json',
+            data,
+            callback
+        });
+    }
 
-  }
-
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static remove(data, callback ) {
-
-  }
+    /**
+     * Удаляет информацию о счёте или доходе/расходе
+     * (в зависимости от того, что наследуется от Entity)
+     * */
+    static remove(id, callback) {
+        createRequest({
+            url: this.URL,
+            method: 'DELETE',
+            responseType: 'json',
+            data: {id},
+            callback
+        });
+        
+    }
 }
