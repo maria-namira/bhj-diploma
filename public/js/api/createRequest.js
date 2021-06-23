0b1047baa3feaca72e269ef6945d436cb55c050f
Maria <mariaes2016@yandex.ru>
2021-06-23 11:11:58 +0300

    diploma

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,49 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const f = function () {},
+        {
+            url = '',
+            method = 'GET',
+            callback = f,
+            responseType = '',
+            async = true,
+            data = {}
+        } = options,
+        xhr = new XMLHttpRequest;
 
+    const formData = new FormData;
+    let params = '';
+
+    if (method === 'GET') {
+        for (param in data) {
+            params += param + '=' + data[param] + '&';
+        }
+        params = '/?' + params.slice(0, -1);
+    } else {
+        for (param in data) {
+            formData.append(param, data[param]);
+        }
+    }
+
+    try {
+        xhr.open(method, url + params);
+        xhr.responseType = responseType;
+        xhr.withCredentials = true;
+        xhr.send(formData);
+    } catch (err) {
+        callback(err);
+    }
+
+    xhr.addEventListener('readystatechange', () => {
+        if (xhr.readyState === xhr.DONE && xhr.status == 200) {
+
+            if (!xhr.response.success) {
+                callback(xhr.response.error, xhr.response);
+            } else {
+                callback(null, xhr.response);
+            }
+
+        }
+    });
 };
