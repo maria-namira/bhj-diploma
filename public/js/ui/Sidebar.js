c2ea87e4545ad068dd4c71a78213eed1b0f422f5
Maria <mariaes2016@yandex.ru>
2021-06-23 10:55:30 +0300

    diploma

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -4,31 +4,46 @@
  * и за кнопки меню
  * */
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
-  static init() {
-    this.initAuthLinks();
-    this.initToggleButton();
-  }
+    /**
+     * Запускает initAuthLinks и initToggleButton
+     * */
+    static init() {
+        this.initAuthLinks();
+        this.initToggleButton();
+    }
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
-  static initToggleButton() {
+    /**
+     * Отвечает за скрытие/показа боковой колонки:
+     * переключает два класса для body: sidebar-open и sidebar-collapse
+     * при нажатии на кнопку .sidebar-toggle
+     * */
+    static initToggleButton() {
+        document.querySelector('a.sidebar-toggle').addEventListener('click', () => {
+            document.querySelector('body').classList.toggle('sidebar-open');
+            document.querySelector('body').classList.toggle('sidebar-collapse');
+        });
+    }
 
-  }
-
-  /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
-   * */
-  static initAuthLinks() {
-
-  }
+    /**
+     * При нажатии на кнопку входа, показывает окно входа
+     * (через найденное в App.getModal)
+     * При нажатии на кнопку регастрации показывает окно регистрации
+     * При нажатии на кнопку выхода вызывает User.logout и по успешному
+     * выходу устанавливает App.setState( 'init' )
+     * */
+    static initAuthLinks() {
+        document.querySelector('.menu-item_login a').addEventListener('click', () => {
+            App.getModal('login').open();
+        });
+        document.querySelector('.menu-item_register a').addEventListener('click', () => {
+            App.getModal('register').open();
+        });
+        document.querySelector('.menu-item_logout a').addEventListener('click', () => {
+            User.logout(User.current(), (err, response) => {
+                if (response.success) {
+                    App.setState('init');
+                }
+            });
+        });
+    }
 }
