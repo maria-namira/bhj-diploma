2c3e3826a7f9ec397faea393b09e6cb937983ea2
Maria <mariaes2016@yandex.ru>
2021-06-28 14:04:04 +0300

    diploma

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -2,20 +2,25 @@
  * Класс CreateAccountForm управляет формой
  * создания нового счёта
  * */
+'use strict';
+
 class CreateAccountForm extends AsyncForm {
-    /**
+ /**
      * Создаёт счёт с помощью Account.create и закрывает
      * окно в случае успеха, а также вызывает App.update()
      * и сбрасывает форму
      * */
-    onSubmit(data) {
-        Account.create(data, (err, response) => {
-            if (response.success) {
-                const regForm = new Modal(this.element.closest('.modal'));
-                regForm.close();
-                this.element.reset();
-                App.update();
-            }
-        });
-    }
-}
+  onSubmit( options) {
+    Account.create(options.data, (err, response) => {
+      if (err ) {
+        console.warn(err);
+        return;
+    };
+                
+          this.element.reset();
+          App.getModal('CreateAccount').close();
+          App.update();
+      });
+    };
+  };
+
