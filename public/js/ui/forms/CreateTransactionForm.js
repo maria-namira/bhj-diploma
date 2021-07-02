8d0cd8d7f3a78ed2362fcdd12a143fc17200bc07
Maria <mariaes2016@yandex.ru>
2021-07-02 19:03:59 +0300

    rewrite

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -1,48 +1,57 @@
-'use strict';
+"use strict";
 
+/**
+ * Класс CreateTransactionForm управляет формой
+ * создания новой транзакции
+ * Наследуется от AsyncForm
+ * */
 class CreateTransactionForm extends AsyncForm {
-  constructor( element ) {
+  /**
+   * Вызывает родительский конструктор и
+   * метод renderAccountsList
+   * */
+  constructor(element) {
     super(element);
     this.renderAccountsList();
-  };
+  }
 
+  /**
+   * Получает список счетов с помощью Account.list
+   * Обновляет в форме всплывающего окна выпадающий список
+   * */
   renderAccountsList() {
-    const selectAccount = this.element.querySelector('.accounts-select');
-
+    const accountsSelect = this.element.querySelector(".accounts-select");
     Account.list(User.current(), (err, response) => {
-      if(err){
-        console.log(err);          
-      return;
-      };
-
-      let accountsData = response.data;
-      selectAccount.innerHTML = '';
-      accountsData.forEach((item) => {
-        selectAccount.insertAdjacentHTML('beforeEnd', `<option value="${item.id}">${item.name}</option>`)
-      })
+      if (response && response.data) {
+        accountsSelect.innerHTML = "";
+        response.data.forEach(item => {
+          accountsSelect.innerHTML += `<option value="${item.id}">${item.name}</option>`;
+        });
+      }
     });
-  };
+  }
 
-  onSubmit( options ) {
+  /**
+   * Создаёт новую транзакцию (доход или расход)
+   * с помощью Transaction.create. По успешному результату
+   * вызывает App.update(), сбрасывает форму и закрывает окно,
+   * в котором находится форма
+   * */
+  onSubmit(options) {
     Transaction.create(options.data, (err, response) => {
-      if (err) {
-        console.log(err);
-        return
-      };
+      if (response && response.success) {
+        const transactionForm = document.querySelectorAll(".modal");
+        transactionForm.forEach(form => {
+          if (form.getAttribute("id") == "modal-new-income") {
+            App.getModal("newIncome").close();
+          } else if (form.getAttribute("id") == "modal-new-expense") {
+            App.getModal("newExpense").close();
+          }
+        });
 
-      this.element.reset();
-
-      if(App.getModal('createExpense')) {
-        App.getModal('createExpense').close();
-      };
-
-      if(App.getModal('createIncome')) {
-        App.getModal('createIncome').close();
-      };
-
-      
-
-      App.update();
-    });
-  };
-};
+        App.update();
+        this.element.reset();
+      }
+    })
+  }
+}
