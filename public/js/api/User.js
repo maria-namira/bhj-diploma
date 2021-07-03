8927234775e51de70b0b938ec6359c2a5da76562
Maria <mariaes2016@yandex.ru>
2021-07-03 18:33:25 +0300

    diplom

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -1,115 +1,118 @@
+"use strict";
+
 /**
  * Класс User управляет авторизацией, выходом и
  * регистрацией пользователя из приложения
  * Имеет свойство URL, равное '/user'.
  * */
 class User {
+  /**
+   * Устанавливает текущего пользователя в
+   * локальном хранилище.
+   * */
+  static URL = "/user";
 
-    static URL = '/user';
+  static setCurrent(user) {
+    localStorage.user = JSON.stringify(user);
+  }
 
-    /**
-     * Устанавливает текущего пользователя в
-     * локальном хранилище.
-     * */
-    static setCurrent(user) {
-        localStorage.setItem('user', JSON.stringify(user));
-    }
+  /**
+   * Удаляет информацию об авторизованном
+   * пользователе из локального хранилища.
+   * */
+  static unsetCurrent() {
+    localStorage.removeItem("user");
+  }
 
-    /**
-     * Удаляет информацию об авторизованном
-     * пользователе из локального хранилища.
-     * */
-    static unsetCurrent() {
-        localStorage.removeItem('user');
-    }
+  /**
+   * Возвращает текущего авторизованного пользователя
+   * из локального хранилища
+   * */
+  static current() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
 
-    /**
-     * Возвращает текущего авторизованного пользователя
-     * из локального хранилища
-     * */
-    static current() {
-     return JSON.parse(localStorage.getItem('user'));
+  /**
+   * Получает информацию о текущем
+   * авторизованном пользователе.
+   * */
+  static fetch(data, callback = f => f) {
+    return createRequest({
+      data,
+      method: "GET",
+      url: this.URL + "/current",
+      responseType: "json",
+      // задаём функцию обратного вызова
+      callback: (err, response) => {
+        if (response && response.user) {
+          User.setCurrent(response.user);
+        } else {
+          User.unsetCurrent();
         }
-    }
+        // вызываем параметр, переданный в метод fetch
+        callback(err, response);
+      }
+    });
+  }
 
-    /**
-     * Получает информацию о текущем
-     * авторизованном пользователе.
-     * */
-    static fetch(callback) {
-        createRequest({
-            url: this.URL + '/current',
-            method: 'GET',
-            responseType: 'json',
-            callback: (err, response) => {
-                if (response && response.user) {
-                    this.setCurrent(response.user);
-                } else {
-                    this.unsetCurrent();
-                }
-                callback(err, response);
-            }
-        });
-    }
+  /**
+   * Производит попытку авторизации.
+   * После успешной авторизации необходимо
+   * сохранить пользователя через метод
+   * User.setCurrent.
+   * */
+  static login(data, callback = f => f) {
+    return createRequest({
+      data,
+      method: "POST",
+      url: this.URL + "/login",
+      responseType: "json",
+      callback: (err, response) => {
+        if (response && response.user) {
+          User.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
+  }
 
-    /**
-     * Производит попытку авторизации.
-     * После успешной авторизации необходимо
-     * сохранить пользователя через метод
-     * User.setCurrent.
-     * */
-    static login(data, callback) {
-        createRequest({
-            url: this.URL + '/login',
-            method: 'POST',
-            responseType: 'json',
-            data,
-            callback: (err, response) => {
-                if (response && response.user) {
-                    this.setCurrent(response.user);
-                }
-                callback(err, response);
-            }
-        });
-    }
+  /**
+   * Производит попытку регистрации пользователя.
+   * После успешной авторизации необходимо
+   * сохранить пользователя через метод
+   * User.setCurrent.
+   * */
+  static register(data, callback = f => f) {
+    return createRequest({
+      data,
+      method: "POST",
+      url: this.URL + "/register",
+      responseType: "json",
+      callback: (err, response) => {
+        if (response && response.user) {
+          User.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
+  }
 
-    /**
-     * Производит попытку регистрации пользователя.
-     * После успешной авторизации необходимо
-     * сохранить пользователя через метод
-     * User.setCurrent.
-     * */
-    static register(data, callback) {
-        createRequest({
-            url: this.URL + '/register',
-            method: 'POST',
-            responseType: 'json',
-            data,
-            callback: (err, response) => {
-                if (response && response.user) {
-                    this.setCurrent(response.user);
-                }
-                callback(err, response);
-            }
-        });
-    }
-
-    /**
-     * Производит выход из приложения. После успешного
-     * выхода необходимо вызвать метод User.unsetCurrent
-     * */
-    static logout(data, callback) {
-        createRequest({
-            url: this.URL + '/logout',
-            method: 'POST',
-            responseType: 'json',
-            data,
-            callback: (err, response) => {
-                if (response && response.success) {
-                    this.unsetCurrent();
-                }
-                callback(err, response);
-            }
-        });
-    }
+  /**
+   * Производит выход из приложения. После успешного
+   * выхода необходимо вызвать метод User.unsetCurrent
+   * */
+  static logout(data, callback = f => f) {
+    return createRequest({
+      data,
+      method: "POST",
+      url: this.URL + "/logout",
+      responseType: "json",
+      callback: (err, response) => {
+        if (response && response.user) {
+          User.unsetCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
+  }
 }
