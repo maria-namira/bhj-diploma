26bfb5ca2a40098eed1082b05c5ffe62241b1645
Maria <mariaes2016@yandex.ru>
2021-07-07 18:46:43 +0300

    diplom

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -10,7 +10,7 @@ class LoginForm extends AsyncForm {
      * закрывает окно, в котором находится форма
      * */
     onSubmit(data) {
-        User.login(data, (err, response) => {
+        User.login((err, response) => {
             if (response.success) {
                 this.element.reset();
                 App.setState('user-logged');
